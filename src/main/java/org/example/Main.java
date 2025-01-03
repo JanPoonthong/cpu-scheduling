092fa62663135eb941771fb5e65a29cc23a26f8d
Jan Poonthong <janpoonthong628@gmail.com>
2025-01-03 10:38:33 +0700

    feat: read input from user

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,39 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int selectedAlgorithm = readInput();
+    }
+
+    /**
+     * This function gets input from the user.
+     *
+     * @return the selected algorithm by user
+     */
+    public static int readInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Pick an algorithm (int): ");
+        int selectedAlgorithm;
+
+        if (scanner.hasNextInt()) {
+            selectedAlgorithm = scanner.nextInt();
+        } else {
+            scanner.next();
+            selectedAlgorithm = -1;
+        }
+
+        while (selectedAlgorithm < 1 || selectedAlgorithm > 4) {
+            System.out.print("Invalid selection of an algorithm, please pick integer from 1 to 4: ");
+            if (scanner.hasNextInt()) {
+                selectedAlgorithm = scanner.nextInt();
+            } else {
+                scanner.next();
+                selectedAlgorithm = -1;
+            }
+        }
+
+        return selectedAlgorithm;
     }
 }
